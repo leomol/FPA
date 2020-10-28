cc760c4dddc828d001855636d6a721d49e125ecc
Leo Molina <leonardomt@gmail.com>
2020-10-28 14:51:24 -0600

    Add loaders

--- common/loadLabChart.m
+++ common/loadLabChart.m
@@ -1,41 +1,18 @@
-% Loads a mat file exported by LabChart in an intuitive manner.
+% LOADLABCHART - Loads a dot-mat file exported from LabChart.
 % 
-% [data, comments, units, titles] = loadLabChart(filename)
+% [time, data, units, names, comments] = loadLabChart(filename)
 % 
-%   Numerical data and comments are organized in 2D structures where row
-%   indices select channels and column indices select blocks:
-%     -Numerical data:
-%        data(channel, block).time
-%        data(channel, block).data
-%       
-%     -Comments:
-%        comments(channel, block).time
-%        comments(channel, block).text
-%   
-%   Units are organized in a cell array where row indices select channels
-%   and column indices select blocks:
-%      -Data units:
-%        units{channel, block}
-%     
-%   Channel titles are organized in a cell array:
-%        titles{channel}
-% 
-% Example:
-%   data = loadLabChart(filename)
-%   plot(data(1, 1).time, data(1, 1).data)
+% See LOADADICHT.
 % 
 % 2020-02-10. Leonardo Molina.
-% 2020-07-22. Last modified.
-function [data, units, names, comments] = loadLabChart(filename)
+% 2020-10-22. Last modified.
+function [time, data, units, names, comments] = loadLabChart(filename)
     vars = load(filename);
     [nChannels, nBlocks] = size(vars.datastart);
     
     % Sort data.
-    data = struct('time', cell(nChannels, nBlocks), 'data', cell(nChannels, nBlocks));
-    empty = repmat({zeros(0, 1)}, nChannels, nBlocks);
-    [data.time] = deal(empty{:});
-    empty = repmat({zeros(0, 1)}, nChannels, nBlocks);
-    [data.data] = deal(empty{:});
+    time = repmat({zeros(0, 1)}, nChannels, nBlocks);
+    data = repmat({zeros(0, 1)}, nChannels, nBlocks);
     offsets = [0; cumsum(seconds(diff(datetime(datevec(vars.blocktimes)))))];
     for c = 1:nChannels
         for b = 1:nBlocks
@@ -43,8 +20,8 @@ function [data, units, names, comments] = loadLabChart(filename)
             if frequency > 0
                 selection = vars.datastart(c, b):vars.dataend(c, b);
                 n = numel(selection);
-                data(c, b).time = offsets(b) + colon(0, n - 1)' / frequency;
-                data(c, b).data = vars.data(selection)';
+                time{c, b} = offsets(b) + colon(0, n - 1)' / frequency;
+                data{c, b} = vars.data(selection)';
             end
         end
     end
@@ -59,11 +36,8 @@ function [data, units, names, comments] = loadLabChart(filename)
     for c = 1:nComments
         text{c} = strtrim(vars.comtext(c, :));
     end
-    comments = struct('time', cell(nChannels, nBlocks), 'text', cell(nChannels, nBlocks));
-    empty = repmat({zeros(0, 1)}, nChannels, nBlocks);
-    [comments.time] = deal(empty{:});
-    empty = repmat({{}}, nChannels, nBlocks);
-    [comments.text] = deal(empty{:});
+    
+    comments = repmat({cell(1, 0)}, nChannels, nBlocks);
     nEntries = size(vars.com, 1);
     for e = 1:nEntries
         channels = vars.com(e, 1);
@@ -74,10 +48,9 @@ function [data, units, names, comments] = loadLabChart(filename)
         for c = channels
             timeIndex1 = vars.com(e, 3);
             timeIndex2 = round(timeIndex1 * vars.samplerate(c, b) / vars.tickrate(b));
-            time = offsets(b) + timeIndex2 / vars.samplerate(c, b);
+            timestamp = offsets(b) + timeIndex2 / vars.samplerate(c, b);
             commentIndex = vars.com(e, 5);
-            comments(c, b).time = cat(1, comments(c, b).time, time);
-            comments(c, b).text = cat(1, comments(c, b).text, text{commentIndex});
+            comments{c, b} = cat(2, comments{c, b}, timestamp, text{commentIndex});
         end
     end
     
